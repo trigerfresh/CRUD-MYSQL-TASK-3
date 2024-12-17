657f37611ee809e02a00f84cc7bbe63984b00f04
Siddhesh Abnave <100532775+trigerfresh@users.noreply.github.com>
2024-12-18 01:03:28 +0530

    Add files via upload

---
+++ frontend/src/data.js
@@ -0,0 +1,57 @@
+const data = {
+  products: [
+    {
+      name: 'Nike Slim Shirt',
+      slug: 'nike-slim-shirt',
+      category: 'Shirts',
+      image: '/images/p1.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      ratings: 4.5,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+
+    {
+      name: 'Adidas Slim Shirt',
+      slug: 'adidas-slim-shirt',
+      category: 'Shirts',
+      image: '/images/p2.jpg',
+      price: 250,
+      countInStock: 20,
+      brand: 'Adidas',
+      ratings: 4.0,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+
+    {
+      name: 'Nike Slim Pant',
+      slug: 'nike-slim-pant',
+      category: 'Pants',
+      image: '/images/p3.jpg',
+      price: 25,
+      countInStock: 15,
+      brand: 'Nike',
+      ratings: 4.5,
+      numReviews: 14,
+      description: 'high quality product',
+    },
+
+    {
+      name: 'Adidas Fit Pant',
+      slug: 'adidas-fir-pant',
+      category: 'Pants',
+      image: '/images/p4.jpg',
+      price: 65,
+      countInStock: 5,
+      brand: 'Adidas',
+      ratings: 4.5,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+  ],
+}
+
+export default data
